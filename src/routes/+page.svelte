2427526b4e2c562a49e498151222cd9bf32001ea
Tegar Aditya <71318821+TegarAditya@users.noreply.github.com>
2024-07-20 11:54:56 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,7 +49,7 @@
 
 		let history = result;
 
-		fetch('http://127.0.0.1:8000/api/history', {
+		fetch('https://calculator-api.tegar.dev/api/history', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded'
