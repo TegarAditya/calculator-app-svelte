d88640023f4de8421478a98101759ee1102d29e8
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-16 11:08:05 +0700

    build: prepare tailwind v4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import { join } from 'path'
 import type { Config } from 'tailwindcss'
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
 
 export default {
 	darkMode: 'class',
@@ -13,15 +12,5 @@ export default {
 	plugins: [
 		forms,
 		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'modern',
-						enhancements: true,
-					},
-				],
-			},
-		}),
 	],
 } satisfies Config;
