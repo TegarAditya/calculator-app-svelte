6380d88f4028751098eb4911de63051fd79e2a4e
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-25 14:56:56 +0700

    refactor: simplify layout by removing unused components and font file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
+	import '@fontsource/quicksand/600.css';
+	import '@fontsource/quicksand/700.css';
 	import '../app.css';
 
-	import 'highlight.js/styles/github-dark.css';
-	import { AppBar } from '@skeletonlabs/skeleton-svelte';
-
-	import LightSwitch from '$lib/components/LightSwitch.svelte';
-	import { Github } from '@lucide/svelte';
+	import TopBar from '$lib/components/TopBar.svelte';
 
 	interface Props {
 		children?: import('svelte').Snippet;
@@ -15,14 +13,6 @@
 </script>
 
 <div class="h-dvh w-full">
-	<AppBar>
-		{#snippet lead()}
-			<LightSwitch />
-		{/snippet}
-		<h1 class="text-xl font-bold">Calculator</h1>
-		{#snippet trail()}
-			<Github class="my-auto"/>
-		{/snippet}
-	</AppBar>
+	<TopBar />
 	{@render children?.()}
 </div>
